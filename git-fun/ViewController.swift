176f41495acb17170329466ebd9f3ba21c118766
Stephan Wegmann <veterinarius@gmx.net>
2016-03-24 19:06:32 +0100

    Initial Commit

---
+++ git-fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git-fun
+//
+//  Created by Stephan Wegmann on 24.03.16.
+//  Copyright © 2016 Stephan Wegmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
