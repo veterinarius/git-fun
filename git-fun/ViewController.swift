ca73bff59e2ba38c925f684e5b4f47011e61fe98
Stephan Wegmann <veterinarius@gmx.net>
2016-03-24 19:11:00 +0100

    Hallo Leute

--- git-fun/ViewController.swift
+++ git-fun/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
+        
+        print("Hallo Leute!")
     }
 
     override func didReceiveMemoryWarning() {
