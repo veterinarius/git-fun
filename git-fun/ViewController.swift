b499a42d87677b62b332c1f419366d0ff7373e75
Stephan Wegmann <veterinarius@gmx.net>
2016-03-24 20:19:35 +0100

    my first commit

--- git-fun/ViewController.swift
+++ git-fun/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib
         
-        print("Hallo Leute!")
+        print("Hallo Fans")
     }
 
     override func didReceiveMemoryWarning() {
